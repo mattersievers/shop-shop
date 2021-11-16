4206f26b8cd39ce00effd005cfdf3b8af4b19014
Matthew Sievers <mattersievers@gmail.com>
2021-11-15 19:03:41 -0600

    started idbPromise

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -4,3 +4,22 @@ export function pluralize(name, count) {
   }
   return name + 's'
 }
+
+export function idbPromise(storeName, method, object) {
+  return new Promise((resolve, reject) => {
+    const request = window.indexedDB.open('shop-shop', 1);
+
+    let db,tx,store;
+
+    request.onupgradeneeded = function (e) {
+      const db = request.result;
+      db.createObjectStore('products', { keyPath: '_id'});
+      db.createObjectStore('categories', { keyPath: '_id'});
+      db.createObjectStore('cart', { keyPath: '_id'});
+    };
+
+    request.onerror = function (e) {
+      console.log('There was an error');
+    };
+  });
+}
